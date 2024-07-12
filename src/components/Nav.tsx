1b162db18adc1eb019ee41b9063fd94ae1d0faaa
Jensone <hello@jensone.com>
2024-07-12 14:55:04 +0200

    desinstallation de react router dom

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,15 +1,14 @@
-import { NavLink } from "react-router-dom";
 import Message from "./icons/MessageIcon";
 
 export default function Nav() {
     return (
         <nav className="flex w-full justify-between items-center">
-            <NavLink to={'/'} className="text-2xl font-semibold text-slate-700">
+            <div  className="text-2xl font-semibold text-slate-700">
                 <h1>lya</h1>
-            </NavLink>
-            <NavLink to={'/messages'} className="">
+            </div>
+            <button className="">
                <Message />
-            </NavLink>
+            </button>
         </nav>
     );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -2,22 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Home from './pages/Home.tsx';
 import './assets/index.css';
-import Messages from './pages/Messages.tsx';
-import { RouterProvider, createBrowserRouter } from 'react-router-dom';
-
-const router = createBrowserRouter([
-    {
-        path: '/',
-        element: <Home />,
-    },
-    {
-        path: '/messages',
-        element: <Messages />,
-    },
-]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
-        <RouterProvider router={router} />
+        <Home />
     </React.StrictMode>
 );
