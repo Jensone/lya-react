11f12a3501697691ce6d69cbfd276789c23c9fe7
Jensone <hello@jensone.com>
2024-07-12 14:22:46 +0200

    first commit

---
+++ src/components/Nav.tsx
@@ -0,0 +1,15 @@
+import { NavLink } from "react-router-dom";
+import Message from "./icons/MessageIcon";
+
+export default function Nav() {
+    return (
+        <nav className="flex w-full justify-between items-center">
+            <NavLink to={'/'} className="text-2xl font-semibold text-slate-700">
+                <h1>lya</h1>
+            </NavLink>
+            <NavLink to={'/messages'} className="">
+               <Message />
+            </NavLink>
+        </nav>
+    );
+}
