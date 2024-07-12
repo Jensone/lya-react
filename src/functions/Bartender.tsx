8238fa554abf355676b21d8a2456b96ba8b22618
Jensone <hello@jensone.com>
2024-07-12 19:04:34 +0200

    🔧 corriger(.gitignore) : ajouter le dossier 'dist' à la liste des fichiers ignorés 🔧 corriger(Chat.tsx) : refactoriser le composant Chat pour utiliser un état local 📦 ajouter(Bartender.tsx) : ajouter la classe Bartender pour gérer les réponses du chat 🔄 refactoriser(Home.tsx) : renommer le composant App en Home et ajuster le contenu 🔄 refactoriser(Messages.tsx) : refactoriser le composant Messages pour utiliser un état local unique pour les chats

---
+++ src/functions/Bartender.tsx
@@ -0,0 +1,22 @@
+import OpenAI from "openai";
+
+export default class Bartender {
+    private openai: OpenAI;
+    private preprompt: string;
+
+    constructor() {
+        this.openai = new OpenAI({
+            apiKey: process.env.REACT_APP_OPENAI_API_KEY || ""
+        });
+        this.preprompt = "En tant que spécialiste en préparation de cocktail. Tu reçois des questions sur la thématique des cocktails, avec tes 20 années d'expérience tu dois répondre par un message simple, vulgarisé et drôle. Voici la question à laquelle tu répondras : ";
+    }
+
+    async askOpenAI(question: string): Promise<string> {
+        const completion = await this.openai.chat.completions.create({
+            model: "gpt-3.5-turbo",
+            messages: [{ role: "system", content: this.preprompt + question + " ?" }],
+        });
+
+        return completion.choices[0].message.content || "";
+    }
+}
