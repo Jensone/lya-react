d293d47faef1ee868ff6b1d4a8b85b1fe357314a
Jensone <hello@jensone.com>
2024-07-13 22:03:15 +0200

    🔥 supprimer(CalendarIcon.tsx) : supprimer l'icône de calendrier inutilisée 🔄 refactoriser(Messages.tsx) : ajouter la gestion du stockage des chats et des fonctionnalités de chargement et de suppression des chats

--- src/pages/Messages.tsx
+++ src/pages/Messages.tsx
@@ -1,31 +1,37 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import Nav from '../components/Nav';
-import EyeIcon from '../components/icons/EyeIcon';
 import TrashIcon from '../components/icons/TrashIcon';
-import CalendarIcon from '../components/icons/CalendarICon';
+import { NavLink } from 'react-router-dom';
+import Storage from '../functions/Storage';
+import BookmarkIcon from '../components/icons/BookmarkIcon';
 
 interface Chat {
-    id: number;
+    id: string;
     question: string;
     answer: string;
-    date: string;
 }
 
+const store = new Storage();
+
 export default function Messages() {
-    const [chats] = useState<Chat[]>([
-        {
-            id: 1,
-            question: "De quoi allons nous parler aujourd'hui ?",
-            answer: "Bonjour, je suis Lya, un IA de conversation. Je suis là pour vous aider à répondre à vos questions et vous fournir des informations utiles. Comment puis-je vous aider aujourd'hui ?",
-            date: '2023-03-01',
-        },
-        {
-            id: 2,
-            question: 'Que souhaites-tu apprendre de nouveau ?',
-            answer: "Je suis là pour vous aider à répondre à vos questions et vous fournir des informations utiles. Comment puis-je vous aider aujourd'hui ?",
-            date: '2023-03-01',
-        },
-    ]);
+    const [chats, setChats] = useState<Chat[]>([]);
+
+    const loadChats = () => {
+        const chats = store.getChats();
+        setChats(chats.map((c) => ({
+            id: c,
+            question: "",
+            answer: ""
+        })));
+    };
+
+    const handleDelete = (id: string) => {
+        store.remove(id);
+    };
+
+    useEffect(() => {
+        loadChats();
+    }, []);
 
     return (
         <main className="bg-slate-50 p-5 flex flex-col items-center gap-4 min-w-full min-h-screen">
@@ -41,17 +47,16 @@ export default function Messages() {
                             key={c.id}
                         >
                             <div className="flex items-center justify-between text-sm uppercase text-slate-400">
-                                <CalendarIcon /> Le {c.date || ''}
+                                <BookmarkIcon /> Le {new Date().toLocaleDateString()}
                             </div>
                             <hr className="my-4" />
                             <div className="italic text-slate-400">
                                 {c.question}
                             </div>
                             <div className="mt-4 flex justify-between">
-                                <button className="bg-blue-500 rounded-full p-2 hover:bg-blue-800">
-                                    <EyeIcon />
-                                </button>
-                                <button className="bg-red-700 rounded-full p-2 hover:bg-red-800">
+                                <button
+                                    onClick={() => handleDelete(c.id)}
+                                    className="rounded-full p-2 hover:bg-red-800">
                                     <TrashIcon />
                                 </button>
                             </div>
