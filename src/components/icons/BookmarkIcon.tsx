15de266e8a4880edc7d66a729e722b218b2ef739
Jensone <hello@jensone.com>
2024-07-13 22:01:18 +0200

    ✨ ajouter(BookmarkIcon.tsx) : ajouter un composant BookmarkIcon ✨ ajouter(EyeIcon.tsx) : mettre à jour les couleurs du composant EyeIcon ✨ ajouter(SendIcon.tsx) : ajouter un composant SendIcon ✨ ajouter(TrashIcon.tsx) : mettre à jour les couleurs du composant TrashIcon

---
+++ src/components/icons/BookmarkIcon.tsx
@@ -0,0 +1,5 @@
+export default function BookmarkIcon() {
+    return (
+        <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 512 512"><path fill="none" stroke="currentColor" strokeLinejoin="round" strokeWidth="32" d="M128 80V64a48.14 48.14 0 0 1 48-48h224a48.14 48.14 0 0 1 48 48v368l-80-64"/><path fill="none" stroke="currentColor" strokeLinejoin="round" strokeWidth="32" d="M320 96H112a48.14 48.14 0 0 0-48 48v352l152-128l152 128V144a48.14 48.14 0 0 0-48-48Z"/></svg>
+    );
+}
