a5a3bec69787cf097f08c2060dcb1bde74fc2821
Jensone <hello@jensone.com>
2024-07-13 22:02:00 +0200

    🔧 corriger(.gitignore) : ajouter le répertoire 'out/' à la liste des fichiers ignorés ✨ fonctionnalité(forge.config.js) : ajouter la configuration pour les makers et les plugins Electron Forge 🔧 corriger(main.js) : ajuster la taille de la fenêtre et ajouter une icône et un style de barre de titre personnalisé

--- main.js
+++ main.js
@@ -1,11 +1,13 @@
 // Importation des modules ElectronJS
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, Tray, Menu, nativeImage } = require('electron');
 const path = require('node:path');
 
 const createWindow = () => {
     const win = new BrowserWindow({
-        width: 800,
-        height: 600,
+        minWidth: 800,
+        minHeight: 600,
+        icon: path.join(__dirname, 'dist/logo.png'),
+        titleBarStyle: 'hidden',
         webPreferences: {
             preload: path.join(__dirname, 'preload.js'),
             openai: path.join(__dirname, 'openai.js')
@@ -18,6 +20,8 @@ const createWindow = () => {
 // Lancez l'application
 app.whenReady().then(() => {
     createWindow()
+    const icon = nativeImage.createFromPath(__dirname, 'dist/logo.png')
+    tray = new Tray(icon)
 });
 
 // Fermez l'application pour Windows et Linux
