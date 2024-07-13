67a1e3c2b3c6e648ee64f1dbc14a8eb578346e39
Jensone <hello@jensone.com>
2024-07-13 22:02:54 +0200

    🔧 corriger(package.json) : mettre à jour les scripts start, preview, package et make 🔧 corriger(package.json) : mettre à jour les dépendances et devDependencies 🔧 corriger(main.tsx) : importer les composants Chat et Messages, mettre à jour le routage

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Home from './pages/Home.tsx';
 import './assets/index.css';
+import { HashRouter, Route, Routes } from "react-router-dom";
+import Chat from './components/Chat.tsx';
+import Messages from './pages/Messages.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-    <React.StrictMode>
-        <Home />
-    </React.StrictMode>
+    <HashRouter>
+        <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/messages" element={<Messages />} />
+            <Route path="/:uuid" element={<Chat />} />
+        </Routes>
+    </HashRouter>
 );
